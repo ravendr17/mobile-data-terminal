8d9f3a8136f78ede17cb20861e4a926aa9a04d87
ravendr17 <ravendr17@gmail.com>
2026-06-19 01:10:23 +0800

    Add initial migration

---
+++ backend/Migrations/20260618170645_InitialCreate.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "account_roles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_account_roles", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "license_statuses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_license_statuses", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "license_types",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_license_types", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "licenses",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    number = table.Column<string>(type: "text", nullable: false),
+                    type_id = table.Column<int>(type: "integer", nullable: false),
+                    status_id = table.Column<int>(type: "integer", nullable: false),
+                    issuance_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    expiration_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    first_name = table.Column<string>(type: "text", nullable: false),
+                    middle_name = table.Column<string>(type: "text", nullable: true),
+                    last_name = table.Column<string>(type: "text", nullable: false),
+                    birth_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    sex = table.Column<string>(type: "text", nullable: false),
+                    address = table.Column<string>(type: "text", nullable: false),
+                    nationality = table.Column<string>(type: "text", nullable: false),
+                    eye_color = table.Column<string>(type: "text", nullable: false),
+                    height = table.Column<int>(type: "integer", nullable: false),
+                    weight = table.Column<int>(type: "integer", nullable: false),
+                    blood_type = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_licenses", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_licenses_license_statuses",
+                        column: x => x.status_id,
+                        principalTable: "license_statuses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_licenses_license_types",
+                        column: x => x.type_id,
+                        principalTable: "license_types",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "accounts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    password = table.Column<string>(type: "text", nullable: false),
+                    role_id = table.Column<int>(type: "integer", nullable: false),
+                    license_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_accounts", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_accounts_account_roles",
+                        column: x => x.role_id,
+                        principalTable: "account_roles",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_accounts_licenses",
+                        column: x => x.license_id,
+                        principalTable: "licenses",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "vehicles",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    plate_number = table.Column<string>(type: "text", nullable: false),
+                    mv_file_number = table.Column<string>(type: "text", nullable: true),
+                    vin = table.Column<string>(type: "text", nullable: false),
+                    register_issuance_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    register_expiry_date = table.Column<DateOnly>(type: "date", nullable: false),
+                    make = table.Column<string>(type: "text", nullable: false),
+                    model = table.Column<string>(type: "text", nullable: false),
+                    year = table.Column<int>(type: "integer", nullable: false),
+                    color = table.Column<string>(type: "text", nullable: false),
+                    license_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_vehicles", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_vehicles_licenses",
+                        column: x => x.license_id,
+                        principalTable: "licenses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_accounts_license_id",
+                table: "accounts",
+                column: "license_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_accounts_role_id",
+                table: "accounts",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_licenses_status_id",
+                table: "licenses",
+                column: "status_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_licenses_type_id",
+                table: "licenses",
+                column: "type_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_vehicles_license_id",
+                table: "vehicles",
+                column: "license_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "accounts");
+
+            migrationBuilder.DropTable(
+                name: "vehicles");
+
+            migrationBuilder.DropTable(
+                name: "account_roles");
+
+            migrationBuilder.DropTable(
+                name: "licenses");
+
+            migrationBuilder.DropTable(
+                name: "license_statuses");
+
+            migrationBuilder.DropTable(
+                name: "license_types");
+        }
+    }
+}
