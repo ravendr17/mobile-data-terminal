427fbdb9a8399b61615a304e0345b4a8946092ee
ravendr17 <ravendr17@gmail.com>
2026-06-19 20:20:18 +0800

    Add StringUtils for Full name

---
+++ backend/Utils/StringUtils.cs
@@ -0,0 +1,11 @@
+namespace Backend.Utils;
+
+public static class StringUtils
+{
+    public static string FullName(string firstName, string? middleName, string lastName)
+    {
+        List<string?> names = [firstName, middleName, lastName];
+
+        return string.Join(" ", names.Where(n => string.IsNullOrWhiteSpace(n)));
+    }
+}
