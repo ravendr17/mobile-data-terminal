1adc6708f50c619e0ac606e972538c78847b0c2e
ravendr17 <ravendr17@gmail.com>
2026-06-23 01:49:15 +0800

    Configure UNIQUE constraints and VARCHAR limits

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -15,54 +15,122 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<Account>()
-            .HasOne(a => a.License)
-            .WithOne()
-            .HasForeignKey<Account>(a => a.LicenseId)
-            .HasConstraintName("fk_accounts_licenses");
-
-        modelBuilder.Entity<Vehicle>()
-            .HasOne(v => v.License)
-            .WithMany(l => l.Vehicles)
-            .HasForeignKey(v => v.LicenseId)
-            .HasConstraintName("fk_vehicles_licenses");
-
-        modelBuilder.Entity<Account>()
-            .HasOne(a => a.Role)
-            .WithMany()
-            .HasForeignKey(a => a.RoleId)
-            .HasConstraintName("fk_accounts_account_roles");
-
-        modelBuilder.Entity<License>()
-            .HasOne(l => l.Type)
-            .WithMany()
-            .HasForeignKey(l => l.TypeId)
-            .HasConstraintName("fk_licenses_license_types");
-
-        modelBuilder.Entity<License>()
-            .HasOne(l => l.Status)
-            .WithMany()
-            .HasForeignKey(l => l.StatusId)
-            .HasConstraintName("fk_licenses_license_statuses");
         
-        modelBuilder.Entity<LicenseType>().HasData(
-            new LicenseType { Id = 1, Name = "Non-Professional" },
-            new LicenseType { Id = 2, Name = "Professional" },
-            new LicenseType { Id = 3, Name = "Student Permit" }
-        );
+        modelBuilder.Entity<License>(entity =>
+        {
+            entity.Property(e => e.Number).HasMaxLength(30);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.MiddleName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+            entity.Property(e => e.Sex).HasMaxLength(10);
+            entity.Property(e => e.Address).HasMaxLength(100);
+            entity.Property(e => e.Nationality).HasMaxLength(30);
+            entity.Property(e => e.EyeColor).HasMaxLength(30);
+            entity.Property(e => e.BloodType).HasMaxLength(10);
 
-        modelBuilder.Entity<LicenseStatus>().HasData(
-            new LicenseStatus { Id = 1, Name = "Active" },
-            new LicenseStatus { Id = 2, Name = "Revoked" },
-            new LicenseStatus { Id = 3, Name = "Expired" }
-        );
+            entity.HasIndex(e => e.Number)
+                .IsUnique().HasDatabaseName("uq_licenses_number");
+            
+            entity.HasOne(l => l.Type)
+                .WithMany()
+                .HasForeignKey(l => l.TypeId)
+                .HasConstraintName("fk_licenses_license_types");
+            
+            entity.HasOne(l => l.Status)
+                .WithMany()
+                .HasForeignKey(l => l.StatusId)
+                .HasConstraintName("fk_licenses_license_statuses");
+        });
 
-        modelBuilder.Entity<AccountRole>().HasData(
-            new AccountRole {Id = 1, Name = "Civilian"},
-            new AccountRole {Id = 2, Name = "Officer"},
-            new AccountRole {Id = 3, Name = "Supervisor"},
-            new AccountRole {Id = 4, Name = "Admin"}
-        );
+        modelBuilder.Entity<Vehicle>(entity =>
+        {
+            entity.Property(e => e.PlateNumber).HasMaxLength(30);
+            entity.Property(e => e.MvFileNumber).HasMaxLength(30);
+            entity.Property(e => e.Vin).HasMaxLength(30);
+            entity.Property(e => e.Make).HasMaxLength(30);
+            entity.Property(e => e.Model).HasMaxLength(30);
+            entity.Property(e => e.Color).HasMaxLength(30);
+            
+            entity.HasIndex(e => e.PlateNumber)
+                .IsUnique()
+                .HasDatabaseName("uq_vehicles_plate_number");
+
+            entity.HasIndex(e => e.MvFileNumber)
+                .IsUnique()
+                .HasDatabaseName("uq_vehicles_mv_file_number");
+            
+            entity.HasIndex(e => e.Vin)
+                .IsUnique()
+                .HasDatabaseName("uq_vehicles_vin");
+            
+            entity.HasOne(v => v.License)
+                .WithMany(l => l.Vehicles)
+                .HasForeignKey(v => v.LicenseId)
+                .HasConstraintName("fk_vehicles_licenses");
+        });
+        
+        
+        modelBuilder.Entity<Account>(entity =>
+        {
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(255);
+
+            entity.HasIndex(e => e.Email)
+                .IsUnique()
+                .HasDatabaseName("uq_accounts_email");
+            
+            entity.HasOne(a => a.Role)
+                .WithMany()
+                .HasForeignKey(a => a.RoleId)
+                .HasConstraintName("fk_accounts_account_roles");
+            
+            entity.HasOne(a => a.License)
+                .WithOne()
+                .HasForeignKey<Account>(a => a.LicenseId)
+                .HasConstraintName("fk_accounts_licenses");
+        });
+        
+        modelBuilder.Entity<AccountRole>(entity =>
+        {
+            entity.Property(e => e.Name).HasMaxLength(30);
+            
+            entity.HasIndex(e => e.Name)
+                .IsUnique().HasDatabaseName("uq_account_roles_name");
+            
+            entity.HasData(
+                new AccountRole { Id = 1, Name = "Civilian" },
+                new AccountRole { Id = 2, Name = "Officer" },
+                new AccountRole { Id = 3, Name = "Supervisor" },
+                new AccountRole { Id = 4, Name = "Admin" }
+            );
+        });
+        
+        modelBuilder.Entity<LicenseType>(entity =>
+        {
+            entity.Property(e => e.Name).HasMaxLength(30);
+            
+            entity.HasIndex(e => e.Name)
+                .IsUnique().HasDatabaseName("uq_license_types_name");
+            
+            entity.HasData(
+                new LicenseType { Id = 1, Name = "Non-Professional" },
+                new LicenseType { Id = 2, Name = "Professional" },
+                new LicenseType { Id = 3, Name = "Student Permit" }
+            );
+        });
+        
+        modelBuilder.Entity<LicenseStatus>(entity =>
+        {
+            entity.Property(e => e.Name).HasMaxLength(30);
+            
+            entity.HasIndex(e => e.Name)
+                .IsUnique().HasDatabaseName("uq_license_statuses_name");
+            
+            entity.HasData(
+                new LicenseStatus { Id = 1, Name = "Active" },
+                new LicenseStatus { Id = 2, Name = "Revoked" },
+                new LicenseStatus { Id = 3, Name = "Expired" }
+            );
+        });
     }
 }
---
+++ backend/Migrations/20260622173826_ConfigureLengthsAndUniques.cs
@@ -0,0 +1,462 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureLengthsAndUniques : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "vin",
+                table: "vehicles",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "plate_number",
+                table: "vehicles",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "mv_file_number",
+                table: "vehicles",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "model",
+                table: "vehicles",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "make",
+                table: "vehicles",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "color",
+                table: "vehicles",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "sex",
+                table: "licenses",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "number",
+                table: "licenses",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "nationality",
+                table: "licenses",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "middle_name",
+                table: "licenses",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "last_name",
+                table: "licenses",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "first_name",
+                table: "licenses",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "eye_color",
+                table: "licenses",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "blood_type",
+                table: "licenses",
+                type: "character varying(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "address",
+                table: "licenses",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "license_types",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "license_statuses",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "accounts",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "accounts",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "account_roles",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "uq_vehicles_mv_file_number",
+                table: "vehicles",
+                column: "mv_file_number",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "uq_vehicles_plate_number",
+                table: "vehicles",
+                column: "plate_number",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "uq_vehicles_vin",
+                table: "vehicles",
+                column: "vin",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "uq_licenses_number",
+                table: "licenses",
+                column: "number",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "uq_license_types_name",
+                table: "license_types",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "uq_license_statuses_name",
+                table: "license_statuses",
+                column: "name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "uq_accounts_email",
+                table: "accounts",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "uq_account_roles_name",
+                table: "account_roles",
+                column: "name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "uq_vehicles_mv_file_number",
+                table: "vehicles");
+
+            migrationBuilder.DropIndex(
+                name: "uq_vehicles_plate_number",
+                table: "vehicles");
+
+            migrationBuilder.DropIndex(
+                name: "uq_vehicles_vin",
+                table: "vehicles");
+
+            migrationBuilder.DropIndex(
+                name: "uq_licenses_number",
+                table: "licenses");
+
+            migrationBuilder.DropIndex(
+                name: "uq_license_types_name",
+                table: "license_types");
+
+            migrationBuilder.DropIndex(
+                name: "uq_license_statuses_name",
+                table: "license_statuses");
+
+            migrationBuilder.DropIndex(
+                name: "uq_accounts_email",
+                table: "accounts");
+
+            migrationBuilder.DropIndex(
+                name: "uq_account_roles_name",
+                table: "account_roles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "vin",
+                table: "vehicles",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "plate_number",
+                table: "vehicles",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "mv_file_number",
+                table: "vehicles",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "model",
+                table: "vehicles",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "make",
+                table: "vehicles",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "color",
+                table: "vehicles",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "sex",
+                table: "licenses",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "number",
+                table: "licenses",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "nationality",
+                table: "licenses",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "middle_name",
+                table: "licenses",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "last_name",
+                table: "licenses",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "first_name",
+                table: "licenses",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "eye_color",
+                table: "licenses",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "blood_type",
+                table: "licenses",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(10)",
+                oldMaxLength: 10);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "address",
+                table: "licenses",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "license_types",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "license_statuses",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "accounts",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "accounts",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "account_roles",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(30)",
+                oldMaxLength: 30);
+        }
+    }
+}
