5b6d0ecb546fc38058f46d96a304b062ff50a2eb
ravendr17 <ravendr17@gmail.com>
2026-06-25 12:56:13 +0800

    Add initial TokenProvider with JwtBearer

---
+++ backend/TokenProvider.cs
@@ -0,0 +1,40 @@
+using System.Security.Claims;
+using System.Text;
+using Backend.Entities;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames;
+
+namespace Backend;
+
+public class TokenProvider(IConfiguration configuration)
+{
+    public string Create(Account account)
+    {
+        string secretKey = configuration["Jwt:secret"]!;
+        
+        var securityKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(secretKey)
+        );
+
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity([
+                new Claim(JwtRegisteredClaimNames.Sub, account.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, account.Email)
+            ]),
+            Expires = DateTime.UtcNow.AddMinutes(configuration.GetValue<int>("Jwt:ExpirationInMinutes")),
+            SigningCredentials = credentials,
+            Issuer = configuration["Jwt:Issuer"],
+            Audience = configuration["Jwt:Audience"]
+        };
+
+        var handler = new JsonWebTokenHandler();
+
+        string token = handler.CreateToken(tokenDescriptor);
+
+        return token;
+    }
+}
