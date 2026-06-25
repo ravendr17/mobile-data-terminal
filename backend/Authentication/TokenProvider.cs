4eff67db90f165343137bc84d9854d966346fcde
ravendr17 <ravendr17@gmail.com>
2026-06-25 22:40:59 +0800

    Remove unecessary long token role field, fix Program.cs' code ordering

--- backend/Authentication/TokenProvider.cs
+++ backend/Authentication/TokenProvider.cs
@@ -24,7 +24,7 @@ public class TokenProvider(IConfiguration configuration)
             Subject = new ClaimsIdentity([
                 new Claim(JwtRegisteredClaimNames.Sub, account.Id.ToString()),
                 new Claim(JwtRegisteredClaimNames.Email, account.Email),
-                new Claim(ClaimTypes.Role, account.Role.Name)
+                new Claim("role", account.Role.Name)
             ]),
             Expires = DateTime.UtcNow.AddMinutes(configuration.GetValue<int>("Jwt:ExpirationInMinutes")),
             SigningCredentials = credentials,
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,31 +1,19 @@
+using System.Text;
 using Backend.Authentication;
 using Backend.Data;
 using Backend.Entities;
 using Backend.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
-
-var dbConnectionStr = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(dbConnectionStr)
-           .UseSnakeCaseNamingConvention()
-);
-
-builder.Services.AddSingleton<PasswordHasher<Account>>();
-builder.Services.AddSingleton<TokenProvider>();
-
-builder.Services.AddScoped<LicenseService>();
-builder.Services.AddScoped<VehicleService>();
-builder.Services.AddScoped<AccountService>();
-
 builder.Services.AddControllers();
+builder.Services.AddOpenApi();
 
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
@@ -45,15 +33,41 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
             {
                 message = $"{field} has an invalid value or format.";
             }
-
             errors[field] = message;
         }
-
         return new BadRequestObjectResult(new { errors });
     };
 });
 
+var dbConnectionStr = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(dbConnectionStr)
+           .UseSnakeCaseNamingConvention()
+);
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(o =>
+    {
+        o.RequireHttpsMetadata = false;
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"]!)
+            ),
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            ClockSkew = TimeSpan.Zero,
+            RoleClaimType = "role"
+        };
+    });
+builder.Services.AddAuthorization();
+
+builder.Services.AddSingleton<PasswordHasher<Account>>();
+builder.Services.AddSingleton<TokenProvider>();
+
+builder.Services.AddScoped<LicenseService>();
+builder.Services.AddScoped<VehicleService>();
+builder.Services.AddScoped<AccountService>();
 
 var app = builder.Build();
 
@@ -63,6 +77,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
