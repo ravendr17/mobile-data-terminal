f617ae51ad781c22934781b215107f7ec5d0ca5b
ravendr17 <ravendr17@gmail.com>
2026-06-30 19:38:20 +0800

    feat: Add username in AccountCreateRequest

--- backend/DTOs/AccountCreateRequest.cs
+++ backend/DTOs/AccountCreateRequest.cs
@@ -3,6 +3,11 @@ using System.ComponentModel.DataAnnotations;
 namespace Backend.DTOs;
 
 public record AccountCreateRequest(
+    [Required(ErrorMessage = "Username is required.")]
+    [MinLength(8, ErrorMessage = "Username must be at least 8 characters long.")]
+    [MaxLength(30, ErrorMessage = "Username cannot exceed 30 characters.")]
+    string Username,
+    
     [Required(ErrorMessage = "Email is required.")] 
     [MaxLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
     [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
