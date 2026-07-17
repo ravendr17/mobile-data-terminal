1e1e14cfdc3c21a64a2c4a8b78db4dbe254d3d13
ravendr17 <ravendr17@gmail.com>
2026-07-17 23:22:00 +0800

    fix null forgiving user role

--- api/Features/Users/User.cs
+++ api/Features/Users/User.cs
@@ -9,7 +9,7 @@ public class User
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public int RoleId { get; set; }
-    public UserRole Role { get; set; }
+    public UserRole Role { get; set; } = null!;
     public int? LicenseId { get; set; }
     public License? License { get; set; }
 }
